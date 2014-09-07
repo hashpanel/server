8c2b7ecac03b828b4e79964e981de4be5de3ba07
Travis Webb <me@traviswebb.com>
2014-09-07 02:31:27 -0400

    removed unused passports

--- config/passport.js
+++ config/passport.js
@@ -20,36 +20,6 @@ module.exports.passport = {
     strategy: require('passport-local').Strategy
   },
 
-  twitter: {
-    name: 'Twitter',
-    protocol: 'oauth',
-    strategy: require('passport-twitter').Strategy,
-    options: {
-      consumerKey: 'your-consumer-key',
-      consumerSecret: 'your-consumer-secret'
-    }
-  },
-
-  github: {
-    name: 'GitHub',
-    protocol: 'oauth2',
-    strategy: require('passport-github').Strategy,
-    options: {
-      clientID: 'your-client-id',
-      clientSecret: 'your-client-secret'
-    }
-  },
-
-  facebook: {
-    name: 'Facebook',
-    protocol: 'oauth2',
-    strategy: require('passport-facebook').Strategy,
-    options: {
-      clientID: 'your-client-id',
-      clientSecret: 'your-client-secret'
-    }
-  },
-
   google: {
     name: 'Google',
     protocol: 'openid',
