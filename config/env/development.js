3948a07d9db0aa997fec4327246211a066ec0c57
Travis Webb <me@traviswebb.com>
2014-09-28 14:31:29 -0400

    npm update

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 /**
- * Development environment settings
+ * Production environment settings
  *
- * This file can include shared settings for a development team,
+ * This file can include shared settings for a production environment,
  * such as API keys or remote database passwords.  If you're using
  * a version control solution for your Sails app, this file will
  * be committed to your repository unless you add it to your .gitignore
@@ -10,31 +10,51 @@
  *
  */
 
+var url = require('url');
+
 module.exports = {
 
   models: {
-    connection: 'localDiskDb',
-    migrate: 'alter'
+    connection: 'herokuPostgres',
+    migrate: 'drop'
   },
 
   connections: {
-    localDiskDb: {
-      adapter: 'sails-disk'
+    herokuPostgres: {
+      adapter: 'sails-postgresql',
+      url: process.env.DATABASE_URL,
+      ssl: true
     },
     sessionCache: {
-      adapter: 'sails-disk'
+      adapter: 'sails-redis',
+      host: url.parse(process.env.REDISCLOUD_URL).hostname,
+      port: url.parse(process.env.REDISCLOUD_URL).port,
+      db: url.parse(process.env.REDISCLOUD_URL).auth.split(":")[0],
+      pass: url.parse(process.env.REDISCLOUD_URL).auth.split(":")[1]
     }
+  },
+
+  session: {
+    adapter: 'redis',
+    host: url.parse(process.env.REDISCLOUD_URL).hostname,
+    port: url.parse(process.env.REDISCLOUD_URL).port,
+    pass: url.parse(process.env.REDISCLOUD_URL).auth.split(":")[1]
+  },
+
+  sockets: {
+    adapter: 'redis',
+    host: url.parse(process.env.REDISCLOUD_URL).hostname,
+    port: url.parse(process.env.REDISCLOUD_URL).port,
+    pass: url.parse(process.env.REDISCLOUD_URL).auth.split(":")[1],
+  },
+  port: process.env.PORT,
+
+  log: {
+    level: 'info'
   },
 
   permissions: {
     adminEmail: 'admin@hashpanel.io',
-    adminPassword: 'admin123'
-  },
-
-  port: 1337,
-
-  log: {
-    level: 'debug'
+    adminPassword: process.env.hashpanel_admin_password
   }
-
 };
