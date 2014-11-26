f2d215f4f05a8d810b7c180f113f7c14a030c651
Travis Webb <me@traviswebb.com>
2014-11-26 18:31:20 -0500

    updated for new sails-auth features

--- config/policies.js
+++ config/policies.js
@@ -19,7 +19,7 @@
 
 module.exports.policies = {
 
-  '*': [ 'passport', 'sessionAuth', 'ModelPolicy', 'OwnerPolicy' ],
+  '*': [ 'basicAuth', 'passport', 'sessionAuth', 'ModelPolicy', 'OwnerPolicy' ],
 
   AuthController: {
     '*': true
